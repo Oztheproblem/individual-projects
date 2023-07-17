ed93557668519d0829928b2ae8adaa3032fefa2b
Osman Khan <osman_khan1987@hotmail.com>
2023-07-17 16:14:35 +0100

    test for withdraw written - on red

--- bank.test.js
+++ bank.test.js
@@ -13,4 +13,10 @@ describe('Bank', () => {
         expect(bank.balance).toBe(1000);
     });
 
+    it('should withdraw money', () => {
+        bank.deposit(1000, '18-07-2023');
+        bank.withdraw(500, '19-07-2023');
+        expect(bank.balance).toBe(500);
+    })
+
 });
