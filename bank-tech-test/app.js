da20e7dfc0202c71c06ab6de3783c752e0e4b5cc
Osman Khan <osman_khan1987@hotmail.com>
2023-07-18 20:12:54 +0100

    integration test added on red, 1st fail

--- bank-tech-test/app.js
+++ bank-tech-test/app.js
@@ -1,14 +1,10 @@
 const Bank = require('./bank');
 
+// create an instance of the Bank class
 const bank = new Bank();
 
-// deposit method called
-bank.deposit(1000, "10-01-2023");
-bank.deposit(2000, "13-01-2023");
+deposit(amount, date)
+// withdraw(amount, date)
+// printSatement()
 
-// withdraw method called
-bank.withdraw(500, "14-07-2023");
-
-//variable statement created to be called in the consolel.log()
-const statement = bank.printStatement();
-console.log(statement);
+module.exports = {deposit, withdraw, printStatement};
---
+++ bank-tech-test/spec/integration.test.js
@@ -0,0 +1,17 @@
+const app = require('../app');
+
+
+describe('Bank', () => {
+    //beforeEach rests bank for each test
+    beforeEach(() => {
+        app.bank.transactions = [];
+        app.bank.balance = 0;
+    });
+    
+    it('should deposit correctly', () => {  
+        app.deposit(1000);
+        expect(app.bank.balance).toBe(1000);
+    });
+
+
+});
