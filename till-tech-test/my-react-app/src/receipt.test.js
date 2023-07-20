37b8491f67e2f6168a6b4fb45db99cb12c248569
Osman Khan <osman_khan1987@hotmail.com>
2023-07-20 16:05:53 +0100

    3 greens for receipt

--- till-tech-test/my-react-app/src/receipt.test.js
+++ till-tech-test/my-react-app/src/receipt.test.js
@@ -7,15 +7,25 @@ import { taxRate, jsonData, calculateOrderTotal } from './receipt';
 
 //simple test to expect that the tax rate is identical to 0.0864
 describe('receipts', () => {
+     // create a mock order to call upon in tests
+    const order = [ { product: 'Cafe Latte', quantity: 2 },];
+
     it('should have the correct tax rate', () => {
        expect(taxRate).toEqual(0.0864);    
     });
 
     it('should calculate the corrct order total price', () => {
         // create a mock order
-        const order = [ { product: 'Cafe Latte', quantity: 2 },];
+        // const order = [ { product: 'Cafe Latte', quantity: 2 },];
 
         const testOrder = calculateOrderTotal(order);
         expect(testOrder).toEqual(4.75 * 2);
+    });
+
+
+    it('should calculate the correct tax rate', () => {
+        const testOrder = calculateOrderTotal(order);
+        const taxAmount = testOrder * 0.0864;
+        expect(taxAmount).toEqual(testOrder * taxRate);
     });
 });
